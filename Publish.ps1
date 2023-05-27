639a81ddf33c05a87bd315bbb969edb562886e23
innovatodev <innovatodev@gmail.com>
2023-05-27 07:39:46 +0200

    1.0.0

---
+++ Publish.ps1
@@ -0,0 +1,4 @@
+﻿New-Item "$env:UserProfile\Documents\WindowsPowerShell\Modules\UUPDumpDownloader" -ItemType Directory
+Copy-Item -Path ".\Module\*" -Destination "$env:UserProfile\Documents\WindowsPowerShell\Modules\UUPDumpDownloader" -Recurse
+Import-Module "$env:UserProfile\Documents\WindowsPowerShell\Modules\UUPDumpDownloader\UUPDumpDownloader.psd1"
+Publish-Module -Name UUPDumpDownloader -NuGetApiKey $env:PSGALLERY_KEY
